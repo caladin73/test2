a05c85738feee4f4bdffbefa5cd5041415b306d4
Peter Myrvig <myrvig@gmail.com>
2017-09-12 10:20:36 +0200

    Merge origin/master into git

--- index.php
+++ index.php
@@ -11,15 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "git push 2";
-        echo 'test';
-        ?>
-         <?php
-        echo "git push 2";
-        ?>
-        
-                 <?php
-        echo "git push 2";
+        echo "new";
         ?>
     </body>
 </html>
